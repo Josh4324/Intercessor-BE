5831319226960461c41873717b8ea8c572ff712b
Josh <adesanyajoshua@ymail.com>
2023-07-16 22:21:53 +0100

    update

--- routes/prayer.js
+++ routes/prayer.js
@@ -5,7 +5,45 @@ const validation = require("../middlewares/validation");
 const { Token } = require("../helpers");
 
 const token = new Token();
-
+/**
+ * @openapi
+ * '/api/v1/post':
+ *  post:
+ *     tags:
+ *     - Post
+ *     summary: Create Post
+ *     requestBody:
+ *        content:
+ *          multipart/form-data:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                image:
+ *                  type: string
+ *                  format: binary
+ *                category:
+ *                  type: string
+ *                  enum: [Announcements, Devotional, Updates, Music Playlist]
+ *                  default: Announcements
+ *                details:
+ *                  type: string
+ *                  default: Post Info
+ *                link:
+ *                  type: string
+ *                  default: johndoe@mail.com
+ *     responses:
+ *      200:
+ *        description: Success
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ *     security:
+ *        - bearerAuth: []
+ *     description: Only an authenticated user can access this route
+ */
 router.post("/", token.verifyToken, prayerController.createPrayer);
 
 router.get("/", token.verifyToken, prayerController.getAllPrayer);
